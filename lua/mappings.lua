d7a235ff4836e61e24fdded4acf431eb517fe8b7
dante <dante@dantes-MacBook-Air.local>
2024-06-03 22:52:39 -0700

    Initial commit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,102 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+local nmap = function(key, effect)
+  vim.keymap.set('n', key, effect, { silent = true, noremap = true })
+end
+
+-- local vmap = function(key, effect)
+--   vim.keymap.set('v', key, effect, { silent = true, noremap = true })
+-- end
+
+local imap = function(key, effect)
+  vim.keymap.set('i', key, effect, { silent = true, noremap = true })
+end
+
+-- local cmap = function(key, effect)
+--   vim.keymap.set('c', key, effect, { silent = true, noremap = true })
+-- end
+
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- From quarto-kickstarter
+
+--- Send code to terminal with vim-slime
+--- If an R terminal has been opend, this is in r_mode
+--- and will handle python code via reticulate when sent
+--- from a python chunk.
+--- TODO: incorpoarate this into quarto-nvim plugin
+--- such that QuartoRun functions get the same capabilities
+--- TODO: figure out bracketed paste for reticulate python repl.
+local function send_cell()
+  if vim.b['quarto_is_r_mode'] == nil then
+    vim.fn['slime#send_cell']()
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.fn['slime#send_cell']()
+  end
+end
+
+--- Send code to terminal with vim-slime
+--- If an R terminal has been opend, this is in r_mode
+--- and will handle python code via reticulate when sent
+--- from a python chunk.
+local slime_send_region_cmd = ':<C-u>call slime#send_op(visualmode(), 1)<CR>'
+slime_send_region_cmd = vim.api.nvim_replace_termcodes(slime_send_region_cmd, true, false, true)
+local function send_region()
+  -- if filetyps is not quarto, just send_region
+  if vim.bo.filetype ~= 'quarto' or vim.b['quarto_is_r_mode'] == nil then
+    vim.cmd('normal' .. slime_send_region_cmd)
+    return
+  end
+  if vim.b['quarto_is_r_mode'] == true then
+    vim.g.slime_python_ipython = 0
+    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
+    if is_python and not vim.b['reticulate_running'] then
+      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
+      vim.b['reticulate_running'] = true
+    end
+    if not is_python and vim.b['reticulate_running'] then
+      vim.fn['slime#send']('exit' .. '\r')
+      vim.b['reticulate_running'] = false
+    end
+    vim.cmd('normal' .. slime_send_region_cmd)
+  end
+end
+
+-- send code with ctrl+Enter
+-- just like in e.g. RStudio
+-- needs kitty (or other terminal) config:
+-- map shift+enter send_text all \x1b[13;2u
+-- map ctrl+enter send_text all \x1b[13;5u
+nmap('<c-cr>', send_cell)
+nmap('<s-cr>', send_cell)
+imap('<c-cr>', send_cell)
+imap('<s-cr>', send_cell)
+
+--- Show R dataframe in the browser
+-- might not use what you think should be your default web browser
+-- because it is a plain html file, not a link
+-- see https://askubuntu.com/a/864698 for places to look for
+local function show_r_table()
+  local node = vim.treesitter.get_node { ignore_injections = false }
+  assert(node, 'no symbol found under cursor')
+  local text = vim.treesitter.get_node_text(node, 0)
+  local cmd = [[call slime#send("DT::datatable(]] .. text .. [[)" . "\r")]]
+  vim.cmd(cmd)
+end
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,32 +7,233 @@ return {
     end,
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+    modules = {
+     ["core.defaults"] = {},
+     ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+     ["core.integrations.nvim-cmp"] = {},
+     ["core.concealer"] = { config = { icon_preset = "diamond" } },
+     ["core.keybinds"] = {
+       -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+       config = {
+         default_keybinds = true,
+         neorg_leader = "<Leader><Leader>",
+       },
+     },
+     ["core.dirman"] = {
+       config = {
+         workspaces = {
+           Journal = "~/Sync/PersonalHome/Journal",
+           org     = "~/Dropbox/org/",
+         }
+       }
+     },
+    },
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua", "html-lsp", "css-lsp" , "prettier"
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
+    "quarto-dev/quarto-nvim",
+    ft = { 'quarto' },
+    dev = false,
+    opts = {
+      lspFeatures = {
+        languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html', 'dot', 'javascript', 'typescript', 'ojs' },
+      },
+      codeRunner = {
+        enabled = true,
+        default_method = 'slime',
+      },
+    },
+    dependencies = {
+      -- for language features in code cells
+      -- configured in lua/plugins/lsp.lua and
+      -- added as a nvim-cmp source in lua/plugins/completion.lua
+      'jmbuhr/otter.nvim',
+    },
+  },
+
+  { -- directly open ipynb files as quarto docuements
+    -- and convert back behind the scenes
+    -- needs:
+    -- pip install jupytext
+    'GCBallesteros/jupytext.nvim',
+    opts = {
+      custom_language_formatting = {
+        python = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto', -- you can set whatever filetype you want here
+        },
+        r = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto', -- you can set whatever filetype you want here
+        },
+      },
+    },
+  },
+
+  { -- send code from python/r/qmd documets to a terminal or REPL
+    -- like ipython, R, bash
+    'jpalardy/vim-slime',
+    dev = false,
+    init = function()
+      vim.b['quarto_is_python_chunk'] = false
+      Quarto_is_in_python_chunk = function()
+        require('otter.tools.functions').is_otter_language_context 'python'
+      end
+
+      vim.cmd [[
+      let g:slime_dispatch_ipython_pause = 100
+      function SlimeOverride_EscapeText_quarto(text)
+      call v:lua.Quarto_is_in_python_chunk()
+      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
+      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      else
+      if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
+      return [a:text, "\n"]
+      else
+      return [a:text]
+      end
+      end
+      endfunction
+      ]]
+
+      vim.g.slime_target = 'neovim'
+      vim.g.slime_no_mappings = true
+      vim.g.slime_python_ipython = 1
+    end,
+    config = function()
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = true
+
+      local function mark_terminal()
+        local job_id = vim.b.terminal_job_id
+        vim.print('job_id: ' .. job_id)
+      end
+
+      local function set_terminal()
+        vim.fn.call('slime#config', {})
+      end
+      vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
+      vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
+    end,
+  },
+
+  { -- paste an image from the clipboard or drag-and-drop
+    'HakonHarnes/img-clip.nvim',
+    event = 'BufEnter',
+    ft = { 'markdown', 'quarto', 'latex' },
+    opts = {
+      default = {
+        dir_path = 'img',
+      },
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = '![$CURSOR]($FILE_PATH)',
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+        quarto = {
+          url_encode_path = true,
+          template = '![$CURSOR]($FILE_PATH)',
+          drag_and_drop = {
+            download_images = false,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('img-clip').setup(opts)
+      vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
+    end,
+  },
+
+  { -- preview equations
+    'jbyuki/nabla.nvim',
+    keys = {
+      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
+    },
+  },
+
+  {
+    'benlubas/molten-nvim',
+    enabled = false,
+    build = ':UpdateRemotePlugins',
+    init = function()
+      vim.g.molten_image_provider = 'image.nvim'
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+    end,
+    keys = {
+      { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
+      {
+        '<leader>mv',
+        ':<C-u>MoltenEvaluateVisual<cr>',
+        mode = 'v',
+        desc = 'molten eval visual',
+      },
+      { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
+    },
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+      preferred_link_style = "markdown"
+    },
+  },
+
 }
