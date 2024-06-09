67f10b23cf5611bbf1a48a71c7de5729184c7d58
dyasui <dtyasui@gmail.com>
2024-06-08 17:49:31 -0700

    added neorg

--- lua/dante/plugins/init.lua
+++ lua/dante/plugins/init.lua
@@ -1,4 +1,8 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  {"vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
 }
